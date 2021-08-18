efdfd7b76238b04961567d006a10f3b00a7f5339
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-18 14:34:19 -0500

    Update shantera.js

--- src/shantera.js
+++ src/shantera.js
@@ -1,5 +1,5 @@
 const shantera = (prefix, pushname) => {
-    return `*Palabras especificas para que el bot les responda*
+    return `*Palabras especificas para que el bot les responda y no seas ignorad@ XD*
 Fiesta del admin
 Admin party
 Viernes
@@ -38,7 +38,9 @@ Un chino
 No funciona
 Boliviano
 Enano
+
 _*Escribe tal y como esta en el mensaje, luego no te quejes que el bot no funciona.*_
+
 By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽`
 
 }
