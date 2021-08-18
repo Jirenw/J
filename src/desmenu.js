21e006624fa215e428aa37e0e6a4b9d3f27744d9
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-18 14:37:58 -0500

    Update desmenu.js

--- src/desmenu.js
+++ src/desmenu.js
@@ -12,18 +12,18 @@ Una alternativa por si *play llego a su limite de canciones.
 ${prefix}play
 Coloca el nombre de la cancion, o el link del video YouTube.
 
-
-${prefix}play Industry Baby Lil Nas X
+_Ejm_
+${prefix}play Faded
 
 ══════════════
 
 *COMUNICADO*
 
-*ytmp4 y *ytmp3 estan en mantenimiento, muy pronto habrá una actualización de esos comandos mas renovado :)
+*ytmp4 y *ytmp3 estan en mantenimiento.
 
 ══════════════
     
-_*play y *play2 tiene un limite de 2000 canciones en el server, si llega a su limite se restablecera dentro de 24 horas_
+_*play y *play2 tiene un limite de 2000 canciones, y se restablece cada 24 horas_
      
 By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽`
 
