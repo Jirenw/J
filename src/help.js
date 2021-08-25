d19bbe49e47613369ee2901ddb8a7f3fb50ed8ca
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-25 14:13:49 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,101 +1,93 @@
 const help = (prefix) => {
 	return `
-*「BoT-☪⃟⚜⃝𝔸ℕ𝕀𝕄𝔼&𝕄𝔸𝕊⃝➳⃟♾」*
 _「By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽」_
+         (҂`_´)
+         <,︻╦̵̵̿╤─ ҉     ~ •••••••           👨🏻‍🦯
+█۞███████]▄▄▄▄▄▄▄▄▄▄▃ ●●●
+▂▄▅█████████▅▄▃▂…
+[███████████████████]
+◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙
+*╚╩══• •✠•INFORMACIÓN•✠ • •══╩╝*
 
-◉ *INFORMACION*
-   ○ COMANDO: ⌜ ${prefix} ⌟
-   ○ GRUPO OFC: ☪⃟⚜⃝𝔸ℕ𝕀𝕄𝔼&𝕄𝔸𝕊⃝➳⃟♾
-   ○ ELLA: NO TE AMA
-   ○ HORARIO DE ACTIVIDAD: 2p.m-5:30a.m 
+ *○ COMANDO:* ⌜ ${prefix} ⌟
+ *○ GRUPO OFC:*☪⃟⚜⃝𝔸ℕ𝕀𝕄𝔼&𝕄𝔸𝕊⃝➳⃟♾
 
-◉ *PARA USAR EL BOT*
-Registrate con el comando ${prefix}daftar y tu Nombre.
-
-◉ *GRUPOS*
-  ║
-  ╠○ ${prefix}closegc
-  ║  ➢Cerrar el grupo. [Solo Admins Pueden Hacerlo]
-  ╠○ ${prefix}opengc
-  ║  ➢Abrir grupo. [Solo Admins Pueden Hacerlo]
-  ╠○ ${prefix}kickmenu
-  ║  ➢Eliminar a un participante.
-  ╠○ ${prefix}promote
-  ║  ➢Dar el honor de ser admin a un participante.
-  ╠○ ${prefix}demote
-  ║  ➢Quitar Admin porque le falta odio :v.
-  ╠○ ${prefix}linkgc
-  ║  ➢Enlace del grupo.
-  ╠○ ${prefix}adminlist
-  ║  ➢Nombra a los administradores del grupo.
-  ╠○ ${prefix}todos
-  ║  ➢Nombra a todos los integrantes del grupo.
-  ╠○ ${prefix}antimenu
-  ║  ➢Antilink's.
-  ╠○ ${prefix}levelmenu
-  ║  ➢Nivel para los grupos.
-  ╠○ ${prefix}welmenu
-  ╚ ➢Comando de Bienvenida al grupo.
-
-◉ *OTROS*
-  ║
-  ╠○ ${prefix}juegos
-  ║  ➢Divierte con tus amigos :)
-  ╠○ ${prefix}desmenu
-  ║  ➢Descargar musica y videos de YouTube.
-  ╠○ ${prefix}version
-  ║  ➢Conoce la versión del bot.
-  ╠○ ${prefix}otak
-  ║  ➢Monas chinas.
-  ╠○ ${prefix}shantera
-  ║  ➢Habla con el bot.
-  ╠○ ${prefix}wame
-  ║  ➢Link de tu Whatsapp.
-  ╠○ ${prefix}qrcode
-  ╚  ➢Coloca un texto
-
-◉ *CREAR STICKERS*
-  ║
-  ╠○ ${prefix}sticker
-  ╠○ ${prefix}attp
-  ║ ➢Mas un texto corto.
-  ╠○ ${prefix}stickergif
-  ╚ ➢6 segundos de video.
-
-◉ *CONVERTIDORES*
-  ║
-  ╠○ ${prefix}toimg
-  ║ ➢De sticker a imagen.
-  ╠○ ${prefix}tomp3
-  ╚ ➢De video a MP3.
-
-◉ *AUDIO*
-  ║
-  ╠○ ${prefix}idioma
-  ╠○ ${prefix}tts es
-  ╚ ➢mas texto.
-  
-◉ *NSWF* 
-  ║
-  ╚○ ${prefix}nsfwmenu
-
-Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
-
+*PARA USAR EL BOT*
+_Registrate con el comando ${prefix}daftar y tu Nombre._
+ ┏━━━━━━━━━━━━━━━━━━━━ 
+ ┃ *┗━┅┅┄⟞⟦GRUPOS⟧⟝┄┉┉━┛*
+ ┣☛ ${prefix}closegc
+ ┃  ⊱Cerrar El Grupo.
+ ┣☛ ${prefix}opengc
+ ┃   ⊱Abrir El Grupo.
+ ┣☛ ${prefix}kickmenu
+ ┃   ⊱Eliminar A Un Participante.
+ ┣☛ ${prefix}promote
+ ┃   ⊱Dar Admin.
+ ┣☛ ${prefix}demote
+ ┃   ⊱Quitar Admin.
+ ┣☛ ${prefix}linkgc
+ ┃   ⊱Enlace Del Grupo.
+ ┣☛ ${prefix}adminlist
+ ┃   ⊱Nombra a L@s Admis Del Grupo.
+ ┣☛ ${prefix}todos
+ ┃   ⊱Nombra A Todos Los Integrantes Del Grupo.
+ ┣☛ ${prefix}antimenu
+ ┃   ⊱Antilink's.
+ ┣☛ ${prefix}levelmenu
+ ┃   ⊱Nivel Para Grupos.
+ ┣☛ ${prefix}welmenu
+ ┃   ⊱Menu De Bienvenida al grupo.
+ ┣☛ ${prefix}nsfwmenu
+ ┃    ⊱Menú De +18.
+ ┣━━━━━━━━━━━━━━━━━━━━ 
+ ┃ *┗━┅┅┄-⟞⟦OTROS⟧⟝-┄┉┉━┛*
+ ┣☛ ${prefix}juegos
+ ┃   ⊱Menú De Juegos.
+ ┣☛ ${prefix}desmenu
+ ┃   ⊱Menu De Descargas.
+ ┣☛ ${prefix}version
+ ┃   ⊱Versión Del Bot.
+ ┣☛ ${prefix}otak
+ ┃   ⊱Monas Chinas.
+ ┣☛ ${prefix}shantera
+ ┃   ⊱Habla Con El Bot.
+ ┣☛ ${prefix}wame
+ ┃   ⊱Link De Tu Whatsapp.
+ ┣☛ ${prefix}qrcode
+ ┃   ⊱Convertir Texto A Código QR.
+ ┣━━━━━━━━━━━━━━━━━━━━ 
+ ┃ *┗━..┄⟞⟦CREAR STICKERS⟧⟝.┄┛*
+ ┣☛ ${prefix}sticker
+ ┣☛ ${prefix}attp
+ ┃    ⊱Mas un texto corto.
+ ┣☛ ${prefix}stickergif
+ ┃    ⊱6 segundos de video.
+ ┣━━━━━━━━━━━━━━━━━━━━ 
+ ┃ *┗━--⟞⟦CONVERTIDORES⟧⟝┉.━┛*
+ ┣☛ ${prefix}toimg
+ ┃    ⊱De Sticker a Imagen.
+ ┣☛ ${prefix}tomp3
+ ┃    ⊱De video a MP3.
+ ┣━━━━━━━━━━━━━━━━━━━━ 
+ ┃ *┗━┅=┅┄⟞⟦AUDIO⟧⟝┄=┉┉━┛*
+ ┣☛ ${prefix}idioma
+ ┣☛ ${prefix}tts es más el texto.
+ ╰━━━━━━━━━━━━━━━━━━━━
 NOTAS.
+➢No escribas al Pv del Bot.
 
 ➢Para que el bot cumpla algunas funciones necesita ser Admin.
 
 ➢No Hagas spam la concha de tu madre, si no responde rápido espera. ;)
 
-➢No entres al grupo ofc a enviar tu enlace no seas Gil(a).
-
-➢Si quieres que el bot esté en tu grupo no hay problema manda mensaje al pv de Jiren. wa.me/+524591347471.
+➢Si quieres que el bot esté en tu grupo o quieres unirte al Grupo Ofc envía mensaje al pv de Jiren. wa.me/+524591347471.
 
 *By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽*
 
-♡━━━━━━━━━━━━━━━━♡━━━━━━━━━━━━━━━♡
+♡━━━━━━━━━━━━♡━━━━━━━━━━━♡
 *⌜🔥♥️Te Amo Wendy♥️🔥, Eres mi complemento Perfecto, Llegaste de la nada y te convertiste en mi todo👩‍❤️‍👨♡My Lady♡⌟*
-♡━━━━━━━━━━━━━━━━♡━━━━━━━━━━━━━━━♡
+♡━━━━━━━━━━━━♡━━━━━━━━━━━♡
 `
 }
 
