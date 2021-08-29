ef2f68bf6e2c111bab472f5fbf317bc3f50a6ef4
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-28 21:45:32 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,6 +1,6 @@
 const help = (prefix) => {
 	return `
-_「By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽」_
+_「〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽」_
           (°~°)
          <,︻╦̵̵̿╤─ ҉     ~  • *ELLA NO TE AMA*
 █۞███████]▄▄▄▄▄▄▄▄▄▄▃ ●●●
@@ -8,86 +8,92 @@ _「By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞�
 [███████████████████]
 *╚╩══✠INFORMACIÓN✠══╩╝*
 
-   *○ COMANDO:* ⌜ ${prefix} ⌟
- *○ GRUPO OFC:*☪⃟⚜⃝𝔸ℕ𝕀𝕄𝔼&𝕄𝔸𝕊⃝➳⃟♾
+*○COMANDO:* ⌜ * ⌟
+*○GRUPO OFC:*     ☪⃟⚜⃝𝔸ℕ𝕀𝕄𝔼&𝕄𝔸𝕊⃝➳⃟♾
+*○ENLACE GRUPO OFC* https://bit.ly/3yt247X
 
-*PARA USAR EL BOT*
-_Registrate con el comando ${prefix}daftar y tu Nombre._
+*◤━━━ ☆. REGISTRATE .☆ ━━━◥*
+*daftar y tu Nombre.
 
  ┏━━━━━━━━━━━━━━━━━━━━ 
  ┃ *┗━┅┅┄⟞⟦GRUPOS⟧⟝┄┉┉━┛*
+ ┣☛ $ { prefix }unir
+ ┃   ๛Agregar Nuevo Participante.
+ ┣☛ $ { prefix }fgc
+ ┃   ๛Cambiar Foto Del Grupo.
+ ┣☛ $ { prefix }ngc
+ ┃   ๛Cambiar Nombre Del Grupo.
+ ┣☛ $ { prefix }dgc
+ ┃   ๛Cambiar Descripcion Del Grupo.
  ┣☛ ${prefix}closegc
- ┃  ⊱Cerrar El Grupo.
+ ┃   ๛Cerrar El Grupo.
  ┣☛ ${prefix}opengc
- ┃   ⊱Abrir El Grupo.
- ┣☛ ${prefix}kickmenu
- ┃   ⊱Eliminar A Un Participante.
- ┣☛ ${prefix}promote
- ┃   ⊱Dar Admin.
- ┣☛ ${prefix}demote
- ┃   ⊱Quitar Admin.
+ ┃   ๛Abrir El Grupo.
+ ┣☛ ${prefix}menú inicial
+ ┃   ๛Eliminar A Un Participante.
+ ┣☛ ${prefix}promover
+ ┃   ๛Dar Admin.
+ ┣☛ ${prefix}degradar
+ ┃   ๛Quitar Admin.
  ┣☛ ${prefix}linkgc
- ┃   ⊱Enlace Del Grupo.
+ ┃   ๛Enlace Del Grupo.
  ┣☛ ${prefix}adminlist
- ┃   ⊱Nombra a L@s Admis Del Grupo.
+ ┃   ๛Nombra a L@s Admis Del Grupo.
  ┣☛ ${prefix}todos
- ┃   ⊱Nombra A Todos Los Integrantes Del Grupo.
+ ┃   ๛Nombra A Todos Los Integrantes Del Grupo.
  ┣☛ ${prefix}antimenu
- ┃   ⊱Antilink's.
+ ┃   ๛Antilink's.
  ┣☛ ${prefix}levelmenu
- ┃   ⊱Nivel Para Grupos.
+ ┃   ๛Nivel Para Grupos.
  ┣☛ ${prefix}welmenu
- ┃   ⊱Menu De Bienvenida al grupo.
+ ┃   ๛Menu De Bienvenida al grupo.
  ┣☛ ${prefix}nsfwmenu
- ┃    ⊱Menú De +18.
+ ┃   ๛Menú De +18.
+ ┣☛ ${prefix}juegos
+ ┃   ๛Menú De Juegos.
  ┣━━━━━━━━━━━━━━━━━━━━ 
  ┃ *┗━┅┅┄-⟞⟦OTROS⟧⟝-┄┉┉━┛*
- ┣☛ ${prefix}juegos
- ┃   ⊱Menú De Juegos.
  ┣☛ ${prefix}desmenu
- ┃   ⊱Menu De Descargas.
+ ┃   ๛Menu De Descargas.
  ┣☛ ${prefix}version
- ┃   ⊱Versión Del Bot.
+ ┃   ๛Versión Del Bot.
  ┣☛ ${prefix}otak
- ┃   ⊱Monas Chinas.
+ ┃   ๛Monas Chinas.
  ┣☛ ${prefix}shantera
- ┃   ⊱Habla Con El Bot.
+ ┃   ๛Habla Con El Bot.
  ┣☛ ${prefix}wame
- ┃   ⊱Link De Tu Whatsapp.
+ ┃   ๛Link De Tu Whatsapp.
  ┣☛ ${prefix}qrcode
- ┃   ⊱Convertir Texto A Código QR.
+ ┃   ๛Convertir Texto A Código QR.
  ┣━━━━━━━━━━━━━━━━━━━━ 
  ┃ *┗━..┄⟞⟦CREAR STICKERS⟧⟝.┄┛*
  ┣☛ ${prefix}sticker
  ┣☛ ${prefix}attp
- ┃    ⊱Mas un texto corto.
+ ┃    ๛Mas un texto corto.
  ┣☛ ${prefix}stickergif
- ┃    ⊱6 segundos de video.
+ ┃    ๛6 segundos de video.
  ┣━━━━━━━━━━━━━━━━━━━━ 
  ┃ *┗━--⟞⟦CONVERTIDORES⟧⟝┉.━┛*
  ┣☛ ${prefix}toimg
- ┃    ⊱De Sticker a Imagen.
+ ┃    ๛De Sticker a Imagen.
  ┣☛ ${prefix}tomp3
- ┃    ⊱De video a MP3.
+ ┃    ๛De video a MP3.
  ┣━━━━━━━━━━━━━━━━━━━━ 
  ┃ *┗━┅=┅┄⟞⟦AUDIO⟧⟝┄=┉┉━┛*
  ┣☛ ${prefix}idioma
  ┣☛ ${prefix}tts es más el texto.
  ╰━━━━━━━━━━━━━━━━━━━━
-NOTAS.
+
+*━━━━▣━━◤NOTAS◢━━▣━━━━*
 ➢No escribas al Pv del Bot.
-
 ➢Para que el bot cumpla algunas funciones necesita ser Admin.
-
 ➢No Hagas spam la concha de tu madre, si no responde rápido espera. ;)
-
 ➢Si quieres que el bot esté en tu grupo o quieres unirte al Grupo Ofc envía mensaje al pv de Jiren. wa.me/+524591347471.
 
+♡━━━━━━━━━━━━♡━━━━━━━━━━♡
+*⌜🔥♥️Te Amo Wendy♥️🔥, Eres mi complemento Perfecto, Llegaste de la nada y te convertiste en mi todo, No te cambio por nada ni por nadie en este mundo👩‍❤️‍👨♡My Lady♡⌟*
+♡━━━━━━━━━━━━♡━━━━━━━━━━♡
 *By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽*
-
-♡━━━━━━━━━━━━♡━━━━━━━━━━━♡
-*⌜🔥♥️Te Amo Wendy♥️🔥, Eres mi complemento Perfecto, Llegaste de la nada y te convertiste en mi todo👩‍❤️‍👨♡My Lady♡⌟*
-♡━━━━━━━━━━━━♡━━━━━━━━━━━♡
 `
 }
 
